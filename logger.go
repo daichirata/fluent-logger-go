38301f437e598c84b097a7a4487d6fe927f56403
Daichi HIRATA <bunny.hop.md@gmail.com>
2017-12-15 14:08:40 +0900

    Fix to write fluent asynchronously

--- logger.go
+++ logger.go
@@ -13,37 +13,48 @@ import (
 )
 
 var (
-	defaultAddress = "127.0.0.1:24224"
+	defaultAddress       = "127.0.0.1:24224"
+	defaultFlushInterval = 5 * time.Second
 )
 
 type Config struct {
 	Address           string
 	ConnectionTimeout time.Duration
+	FlushInterval     time.Duration
 }
 
 func withDefaultConfig(c Config) Config {
 	if c.Address == "" {
 		c.Address = defaultAddress
 	}
+	if c.FlushInterval == 0 {
+		c.FlushInterval = defaultFlushInterval
+	}
 	return c
 }
 
 type Logger struct {
-	conf Config
-	conn io.WriteCloser
-	bmu  sync.Mutex
-	cmu  sync.Mutex
-	buf  []byte
+	conf  Config
+	conn  io.WriteCloser
+	bmu   sync.Mutex
+	cmu   sync.Mutex
+	buf   []byte
+	wg    sync.WaitGroup
+	done  chan struct{}
+	dirty chan struct{}
 }
 
 func NewLogger(c Config) (*Logger, error) {
 	logger := &Logger{
-		conf: withDefaultConfig(c),
-		buf:  []byte{},
+		conf:  withDefaultConfig(c),
+		buf:   []byte{},
+		done:  make(chan struct{}),
+		dirty: make(chan struct{}),
 	}
 	if err := logger.connect(); err != nil {
 		return nil, err
 	}
+	logger.start()
 	return logger, nil
 }
 
@@ -69,10 +80,14 @@ func (logger *Logger) PostWithTime(tag string, t time.Time, obj interface{}) err
 	logger.buf = append(logger.buf, raw...)
 	logger.bmu.Unlock()
 
-	return logger.send()
+	go func() {
+		logger.dirty <- struct{}{}
+	}()
+	return nil
 }
 
 func (logger *Logger) Close() error {
+	logger.stop()
 	return logger.disconnect()
 }
 
@@ -136,4 +151,27 @@ func (logger *Logger) send() error {
 	}
 
 	return err
+}
+
+func (logger *Logger) start() {
+	ticker := time.NewTicker(logger.conf.FlushInterval)
+	logger.wg.Add(1)
+	go func() {
+		defer logger.wg.Done()
+		for {
+			select {
+			case <-logger.done:
+				logger.send()
+				return
+			case <-logger.dirty:
+			case <-ticker.C:
+			}
+			logger.send()
+		}
+	}()
+}
+
+func (logger *Logger) stop() {
+	close(logger.done)
+	logger.wg.Wait()
 }
